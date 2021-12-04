315a3ad878332cf41b0b57c743dfb1ce319a57b6
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2021-12-04 20:12:14 +0000

    day 4 working solution

--- solutions/day_4.py
+++ solutions/day_4.py
@@ -31,47 +31,41 @@ def problem_1():
     if len(b) > 0:
       winning_board = grid[b[0]]
       winning_board_sum = np.sum(winning_board.flatten())
-      print(winning_board_sum, number, winning_board_sum*number)
+      print(winning_board)
+      print(winning_board_sum*number)
       break
 
 def problem_2():
-  grid = np.array([[[1,1,0],[1,2,0],[4,5,0]], [[0,0,1],[1,2,0],[4,5,1]]], "int")
-  # b = np.where(~grid.any())[0]
-  # print(b)
+  numbers, boards = read_input()
+  grid = np.array(boards, dtype="int")
 
-  # to check both rows and columns, create a transposed instance as well
-  for board in grid:
-    transposed_board = board.transpose()
-    a = np.where(~board.any(axis=1))[0]
-    b = np.where(~transposed_board.any(axis=1))[0]
-    
-    if len(a) > 0:
-      print("original: ",board)
-    
-    if len(b) > 0:
-      print("transposed: ",transposed_board)
-
-
-  # b = np.where(~transposed_grid.any())[0]
-  # print(b)
-
-  # numbers, boards = read_input()
+  # convert all 0 to 100. We'll be using 0 to figure out which number is called
+  grid[grid == 0] = 100
   
-  # # convert the boards to a numpy grid for easier searching, replacing
-  # grid = np.array(boards, dtype="int")
+  winning_boards = []
 
-  # # start the bingo!
-  # for number in numbers:
-  #   # with thanks to https://stackoverflow.com/a/19666680
-  #   grid[grid == number] = "0"
-  #   b = np.where(~grid.any(axis=1))[0]#
-  #   if len(b) > 1:
-  #     print(grid[b[0]])
-  #     print(grid[b[1]])
-  #     break
-  #     # winning_board = grid[b[0]]
-  #     # winning_board_sum = np.sum(winning_board.flatten())
-  #     # print(winning_board_sum, number, winning_board_sum*number)
-  #     # break
+  # start the bingo!
+  for number in numbers:
 
-  
+    # all 0 has been converted to 100
+    if number == 0:
+      number = 100
+
+    grid[grid == number] = 0
+
+    for i,board in enumerate(grid):
+      # to check both rows and columns, create a transposed instance as well
+      transposed_board = board.transpose()
+
+      # with thanks to https://stackoverflow.com/a/19666680
+      a = np.where(~board.any(axis=1))[0]
+      b = np.where(~transposed_board.any(axis=1))[0]
+
+      if len(a) > 0 or len(b) > 0:
+        # store the winning board along with the number which triggered it
+        winning_boards.append((np.copy(board), number))
+        grid[i] = -1
+
+  last_winning_board_sum = np.sum(winning_boards[-1][0].flatten()) * winning_boards[-1][1]
+  print(winning_boards[-1][0])
+  print(last_winning_board_sum)
